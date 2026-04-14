f2a16e907126c1d72349004350f8efaecc258bd3
H3rC4 <hernanenriquecaballero@gmail.com>
2026-04-14 17:40:15 +0200

    fix: import missing cookies

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@/lib/supabase/server'
+import { cookies } from 'next/headers'
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
